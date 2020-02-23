f153a8817b6932496ade076e4b6dfbd489b9ff74
John Zaiss <jzaiss1@live.maryville.edu>
2020-02-23 16:59:26 -0600

    starting

---
+++ itemsDB.py
@@ -0,0 +1,47 @@
+# John Zaiss (GitHub id jzaiss1)
+# SWDV 630: OBJECT-ORIENTATED CODING 1W 20/SP1
+# Week 6 Assignment - Storing Objects in the Database
+
+import sqlalchemy
+
+class Item:
+  def __init__(self, name, price):
+    self.name = name
+    self.price = price
+    self.quantity = 1
+    # TODO: Increment the quantity
+  
+  def __str__(self):
+        return "{}: $ {}".format(self.name, self.price)
+
+class OrderIterator:
+  def __init__(self, items):
+    self.indx = 0
+    self.items = items
+
+  def has_next(self):
+    return False if self.indx >= len(self.items) else True
+
+  def next(self):
+    item = self.items[self.indx]
+    self.indx += 1
+    return item
+
+  def remove(self):
+    return self.items.pop()
+
+class Sale:
+  def __init__(self):
+    self.items = []
+  
+  def add(self, item):
+    self.items.append(item)
+  
+  def iterator(self):
+    return OrderIterator(self.items)
+
+
+def main():
+  sqlalchemy.__version__ 
+
+main()
